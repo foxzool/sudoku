cdffbb7f2799adbb366601b6df89c90ab9f04cf4
ZoOL <zhooul@gmail.com>
2023-12-29 18:46:53 +0800

    add dev feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "Bevy sudoku game".to_string(),
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy sudoku game".to_string(),
+                ..Default::default()
+            }),
             ..Default::default()
-        }),
-        ..Default::default()
-    })).run();
+        }))
+        .run();
 }
